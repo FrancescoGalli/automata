2964e62caa5c667f751afba7891f98f66780414b
Francesco <francesco.galli13@studio.unibo.it>
2024-08-26 17:52:44 +0200

    initialization function implementation

--- automata.py
+++ automata.py
@@ -5,9 +5,9 @@ def evolution(old_state):
     for i in range(len(old_state)-2):
         if (old_state[i+1]<1 and (old_state[i]==old_state[i+2])) or (
             old_state[i]>0 and old_state[i+1]>0):
-                new_state[i+1]=0
+                new_state[i+1] = 0
         else:
-             new_state[i+1]=1
+             new_state[i+1] = 1
     return new_state
 
 def simulation(initial_state, ngen):
@@ -17,8 +17,17 @@ def simulation(initial_state, ngen):
           state=evolution(state)
           print(state)
 
+def initialization(length, one_positions):
+    initial_state=np.zeros(length)
+    for i in range(len(one_positions)):
+        initial_state[one_positions[i]] = 1
+    return initial_state
 
-ngen=20
-initial_state=([0, 0, 0, 0, 1, 0, 0, 0, 0])
+length=9
+mid_position=int(length/2)
+one_positions=([mid_position, 6])
+ngen=3
+
+initial_state = initialization(length, one_positions)
 simulation(initial_state, ngen)
 
